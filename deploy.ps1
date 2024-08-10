aea804d9a3344bda95c0682084d488aa1506f5ab
Greg Thomas Andes <andesgregthomas@gmail.com>
2024-08-10 09:53:44 +0800

    debugging: workflow successful but not creating AWS stack

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,16 @@
+# $StackName = 'awsvpcstack'
+# $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
+
+# New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+
 $StackName = 'awsvpcstack'
 $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
 
-New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+try {
+    $result = New-CFNStack -StackName $StackName -TemplateBody $TemplateBody -ErrorAction Stop
+    Write-Output "Stack creation initiated. Stack ID: $($result.StackId)"
+}
+catch {
+    Write-Error "Failed to create stack: $_"
+    exit 1
+}
