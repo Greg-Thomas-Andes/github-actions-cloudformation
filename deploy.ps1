baaa02145902ebe46f13494ad48b97f8d3f1e71a
Greg Thomas Andes <andesgregthomas@gmail.com>
2024-08-10 10:00:32 +0800

    resolve the issue with the unrecognized cmdlet

--- deploy.ps1
+++ deploy.ps1
@@ -1,12 +1,15 @@
-# $StackName = 'awsvpcstack'
-# $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
-
-# New-CFNStack -StackName $StackName -TemplateBody $TemplateBody
+# Import the module
+Import-Module AWS.Tools.CloudFormation
 
 $StackName = 'awsvpcstack'
 $TemplateBody = Get-Content -Path cloudformation-vpc.yml -Raw
 
 try {
+    # Check if the cmdlet exists
+    if (-not (Get-Command New-CFNStack -ErrorAction SilentlyContinue)) {
+        throw "New-CFNStack cmdlet not found. Make sure AWS.Tools.CloudFormation is properly installed and imported."
+    }
+
     $result = New-CFNStack -StackName $StackName -TemplateBody $TemplateBody -ErrorAction Stop
     Write-Output "Stack creation initiated. Stack ID: $($result.StackId)"
 }
